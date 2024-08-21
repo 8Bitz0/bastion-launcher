c7a74e9ad027d852f39ebb2330e552d9d5c65f9e
Alexander Hansen <alex@bitzmc.com>
2024-08-21 11:21:14 -0400

    Change indentation inside `RadialSetupButton.svelte` to 2 spaces

--- src/routes/RadialSetupButton.svelte
+++ src/routes/RadialSetupButton.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    import { Icon, type IconSource } from "svelte-hero-icons";
+  import { Icon, type IconSource } from "svelte-hero-icons";
 
-    let className: string = '';
-    export let label: string;
-    export let icon;
+  let className: string = '';
+  export let label: string;
+  export let icon;
 
-    export { className as class };
+  export { className as class };
 </script>
 
 <div class="flex flex-col p-4 justify-center items-center {className}">
-    <button class="w-10 h-10 rounded-full border-white border-2">
-        <Icon src="{icon}" class="text-white scale-[70%]" />
-    </button>
-    <p class="font-sans">{label}</p>
+  <button class="w-10 h-10 rounded-full border-white border-2">
+    <Icon src="{icon}" class="text-white scale-[70%]" />
+  </button>
+  <p class="font-sans text-xs">{label}</p>
 </div>
